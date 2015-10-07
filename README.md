877537504390424aae5197de462b2da72c67a73b
frd devel <devel.fd@laposte.net>
2015-10-08 00:03:34 +0200

    initial version of project 'mybooks'

---
+++ README.md
@@ -0,0 +1,5 @@
+# OC-MyBooks
+
+Code source de l'activité 1 du cours OpenClassrooms "Evoluez vers une architecture PHP professionnelle".
+
+Ma version
---
+++ src/Domain/Book.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Mybooks\Domain;
+
+class Book 
+{
+    /**
+     * Book id.
+     *
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * Book title.
+     *
+     * @var string
+     */
+    private $title;
+
+    /**
+     * Book content.
+     *
+     * @var string
+     */
+    private $summary;
+
+    /**
+     * Book author id.
+     *
+     * @var integer
+     */
+    private $auth_id;
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function setTitle($title) {
+        $this->title = $title;
+    }
+
+    public function getSummary() {
+        return $this->summary;
+    }
+
+    public function setSummary($summary) {
+        $this->summary = $summary;
+    }
+
+    public function getAuth_id() {
+        return $this->auth_id;
+    }
+
+    public function setAuth_id($auth_id) {
+        $this->auth_id = $auth_id;
+    }
+}
